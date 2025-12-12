fb76f3ea8d8e087bf2148f4fb8bde8bd1931af36
Anirudh.PVS <80063053+Anirudh-ui@users.noreply.github.com>
2025-12-12 17:12:37 +0530

    Remove GoogleGenerativeAIEmbeddings usage from tests

--- test_conn.py
+++ test_conn.py
@@ -1,3 +1,1 @@
-from langchain_google_genai import GoogleGenerativeAIEmbeddings
-emb = GoogleGenerativeAIEmbeddings(model="models/text-embedding-004", google_api_key="AIzaSyBzotrB7B4c1I2N_7IuvG4Bnl7yCl4hbMw")
-print(emb.embed_query("hello world"))
+
